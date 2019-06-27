73d95826b1881e52a52391fe110f35fcda40a0b9
Alexa Morales <alexamorales@fb.com>
2019-06-26 18:06:48 -0700

    Table view

---
+++ Flix/MoviesViewController.h
@@ -0,0 +1,17 @@
+//
+//  MoviesViewController.h
+//  Flix
+//
+//  Created by alexamorales on 6/26/19.
+//  Copyright © 2019 alexamorales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoviesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
